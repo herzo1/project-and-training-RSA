79b4ee7a31c36ada04588e2aea3ca86cbbb829c2
herzo1 <o.herzig95@gmail.com>
2020-11-21 14:49:09 +0100

    init classes

---
+++ src/bfh/pt2/mathematik/Main.java
@@ -0,0 +1,7 @@
+package bfh.pt2.mathematik;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/bfh/pt2/mathematik/RSA.java
@@ -0,0 +1,19 @@
+package bfh.pt2.mathematik;
+
+public class RSA {
+    /*
+     * Following constants are from the Mail from daniel.tokarev@bfh.ch from 01.11.2020
+     */
+    private static final int P_1 = 13451;
+    private static final int Q_1 = 2621;
+    private static final int E_1 = 3;
+    private static final int P_2 = 1451;
+    private static final int Q_2 = 15161;
+    private static final int N = 77526089;
+    private static final int E_2 = 5;
+
+    private static final String M_1 = "needed to";
+    private static final int[] M_2 = {1560896, 1481544, 912673, 1331000, 1520875, 1295029, 1157625, 1560896};
+    private static final int[] M_3 = {64120834, 17019509, 25668535, 32627809, 18145261};
+
+}
